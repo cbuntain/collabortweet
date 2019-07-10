ef7ed6843ac537fe7458e2daa5370be1ce0681f8
Fridolin Linder <fridolin.linder@nyu.edu>
2019-07-10 15:43:11 +0200

    Refactored retrieval script
    - summary stats are now much more readable
    - Works in more general scenarios (without gold standard coder and with
    differing amounts of overlap between coders)

--- bin/retrieve_partitioned_task.py
+++ bin/retrieve_partitioned_task.py
@@ -1,20 +1,27 @@
 '''
 This script extracts labels from the database and evaluates coder quality
-using all labeled elements that overlap between all coders. If gold standard
-codeer is provided accuracy in revovering that coder's labels is reported as well.
+using all labeled elements that overlap between all coders.
+
+Author: Fridolin Linder
 
 Arguments:
     db_path: str, path of the sqlite databasefile
     task_ids: iterable of integers, Collabortweet task ID(s)
-    multi_label_resolution: str, method to use to assign a label to each element if there are
-        multiple labels for that element. Default is plurality (i.e. the label that has most
-        'votes', in case of ties a random label is choosen from the tied ones) can also be the
-        screenname of one of the coders (e.g. if a gold standard coder is proved), in that case
-        that coder's labels are used for all overlapping elements.
-    gold_standard_name: str, Collabortweet screenname for the gold standard coder
+    gold_standard_coder: str, Collabortweet screenname for the gold standard coder. If omitted,
+        no evaluation is done and all labeled elements are stored in `output`.
     output: str, path where to store the output csv file
     time_cutoff: int, optional, seconds to use as cutoff for the labeling time for each element
         (default is 60*3)
+
+Returns:
+    Prints evaluation metrics and summary stats for every coder. If gold standard coder is provided
+    (`--gold_standard_coder`) accuracy in recovering this coder's labels is reported as well.
+    The curated data is returned in csv format with columns:
+        - 'externalId': Id of the element
+        - 'label': Label given by coder (if multiple coders labled the element the label according
+           to plurality vote is given)
+        - 'entropy': The shannon entropy of the labels for elements with multiple coders, NA for
+           elements with single coder
 '''
 
 import itertools
@@ -27,14 +34,21 @@ from statsmodels.stats.inter_rater import cohens_kappa
 from sklearn.metrics import confusion_matrix, accuracy_score
 from scipy.stats import entropy
 
+def print_summary(title, data):
+    print('+'*80)
+    print(f'{title}:')
+    print('+'*80)
+    print(data)
+    print('+'*80)
+    print('\n')
+
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # Parse input arguments
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 parser = argparse.ArgumentParser()
 parser.add_argument('--db_path', type=str)
 parser.add_argument('--task_ids', nargs='+', type=int)
-parser.add_argument('--multi_label_resolution', type=str, default='majority')
-parser.add_argument('--gold_standard_name', type=str, default=None)
+parser.add_argument('--gold_standard_coder', type=str, default=None)
 parser.add_argument('--output', type=str)
 parser.add_argument('--time_cutoff', type=int, default=180)
 args = parser.parse_args()
@@ -42,9 +56,14 @@ args = parser.parse_args()
 DB_PATH = args.db_path
 CUTOFF = args.time_cutoff
 TASK_IDS = args.task_ids
-EVAL_USER = args.gold_standard_name
+EVAL_USER = args.gold_standard_coder
 OUTPATH = args.output
+
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# Don't truncate pandas dataframes when printing coder stats
+pd.set_option('display.max_colwidth', -1)
+pd.set_option('display.max_columns', None)
 
 # Get the raw labels from the database
 db_connection = sqlite3.connect(DB_PATH)
@@ -69,42 +88,26 @@ labels = pd.read_sql_query(query, db_connection)
 labels = labels[~labels.duplicated(subset=['externalId', 'screenname'])]
 coders = labels['screenname'].unique()
 
-# Find the eval tweets (tweets that are overlapping between all coders)
-label_counts = labels[['externalId', 'screenname']].groupby('externalId').count()
-eval_ids = label_counts[label_counts['screenname'] == len(coders)].index
-
-eval_df = labels.pivot(index='externalId', columns='screenname', values='labelText')
-eval_df = eval_df.loc[eval_ids]
-
-# Calculate cohens cappa for all coder pairs
-kappas = []
-for coder_pair in itertools.combinations(coders, 2):
-    cm = confusion_matrix(eval_df[coder_pair[0]], eval_df[coder_pair[1]])
-    kappa = cohens_kappa(cm)
-    kappas.append((*coder_pair, kappa['kappa']))
-kappas = pd.DataFrame(kappas, columns=['coder_1', 'coder_2', 'kappa'])
-
-avg_kappas = []
+# Number of labels per coder and overlap with every other coder
+ccols = labels[['externalId', 'screenname', 'labelText']].pivot(
+    index='externalId', columns='screenname', values='labelText'
+)
+ccols = ccols.notna()
+out = []
 for coder in coders:
-    val = kappas[(kappas['coder_1'] == coder) |
-                 (kappas['coder_2'] == coder)]['kappa'].mean()
-    avg_kappas.append((coder, val))
-avg_kappas = pd.DataFrame(avg_kappas, columns=['coder', 'avg_kappa'])
-avg_kappas.set_index('coder', inplace=True, verify_integrity=True)
-
-# If gold standard coder is given, calculate agreement kappa and %agreement
-gs_agreement = []
-if EVAL_USER is not None:
-    eval_labels = eval_df[EVAL_USER]
-    for coder in coders:
-        if coder == EVAL_USER:
-            continue
-        val = accuracy_score(eval_labels, eval_df[coder])
-        gs_agreement.append((coder, val))
-    gs_agreement = pd.DataFrame(
-        gs_agreement, columns=['coder', 'gold_standard_accuracy']
-    )
-    gs_agreement.set_index('coder', inplace=True, verify_integrity=True)
+    tempdf = ccols[ccols[coder]]
+    o = tempdf.sum(axis=0)
+    o['screenname'] = coder
+    out.append(o)
+sumdf = pd.DataFrame(out)
+sumdf = sumdf[['screenname'] + list(coders)]
+sumdf.set_index('screenname', inplace=True, drop=True)
+for i in range(0, sumdf.shape[0]):
+    for j in range(0, sumdf.shape[1]):
+        if i > j:
+            sumdf.iloc[i, j] = np.nan
+sumdf['total'] = sumdf.max(axis=0)
+print_summary('Number of coded elements per coder and overlap with other coders', sumdf)
 
 # Calculate average time per tweet for each labeler
 labels['time_delta'] = pd.to_datetime(labels['time']).diff()
@@ -117,33 +120,85 @@ time_df = time_df[time_df['time_delta'] <= CUTOFF]
 time_df = time_df[['screenname', 'time_delta']].groupby('screenname')\
                                                .agg(['median', 'mean'])
 
-time_df.columns = ['median_time', 'mean_time']
+time_df.reset_index(inplace=True, drop=False)
+time_df.columns = ['coder', 'median_time', 'mean_time']
+time_df.set_index('coder', drop=True, inplace=True)
+print_summary('Time spent on coding task', time_df)
 
-## Get the number of elements each coder labeled
-## eval and non_eval
-labels['is_eval'] = labels['externalId'].isin(eval_ids)
-n_labels = labels[['screenname', 'is_eval', 'labelText']].groupby(
-    ['screenname', 'is_eval']
-).count()
-n_labels.reset_index(inplace=True)
-n_labels = n_labels.pivot(
-    index='screenname', columns='is_eval', values='labelText'
-)
-n_labels.columns = ['unique_elements', 'overlapping_elements']
+# Calculate cohens cappa for all coder pairs
+kappas = []
+for coder_pair in itertools.combinations(coders, 2):
+    tempdf = labels[labels['screenname'].isin(coder_pair)]
+    label_counts = tempdf[['externalId', 'screenname']].groupby('externalId').count()
+    overlapping_ids = label_counts[label_counts['screenname'] == 2].index
+    if len(overlapping_ids) == 0:
+        kappa = np.nan
+    else:
+        eval_df = tempdf.pivot(index='externalId', columns='screenname', values='labelText')
+        eval_df = eval_df.loc[overlapping_ids]
+        cm = confusion_matrix(eval_df[[coder_pair[0]]], eval_df[[coder_pair[1]]])
+        kappa = cohens_kappa(cm)
+        kappas.append((*coder_pair, kappa['kappa'], len(overlapping_ids)))
+kappas = pd.DataFrame(kappas, columns=['coder_1', 'coder_2', 'kappa', 'overlapping_elements'])
+kappas.set_index(['coder_1', 'coder_2'], drop=True, inplace=True)
+print_summary("Cohen's Kappa for all coder pairs", kappas)
 
-coder_stats = avg_kappas.join(time_df).join(n_labels)
+avg_kappas = []
+kappas.reset_index(drop=False, inplace=True)
+for coder in coders:
+    val = kappas[(kappas['coder_1'] == coder) |
+                 (kappas['coder_2'] == coder)]['kappa'].mean()
+    avg_kappas.append((coder, val))
+avg_kappas = pd.DataFrame(avg_kappas, columns=['coder', 'avg_kappa'])
+avg_kappas.set_index('coder', inplace=True, verify_integrity=True)
+print_summary("Average Cohen's Kappa by coder", avg_kappas)
 
-# Join it all into one table
 if EVAL_USER is not None:
-    coder_stats = coder_stats.join(gs_agreement)
+    # Give overview of how many tweets were labeled by each coder + overlap with Gold standard
+    gs_ids = set(labels[labels['screenname'] == EVAL_USER]['externalId'])
+    out = []
+    for coder in coders:
+        if coder == EVAL_USER:
+            continue
+        # Get the number of labels provided by this coder
+        coder_ids = set(labels[labels['screenname'] == coder]['externalId'])
 
-## Print summary stats
-print(coder_stats)
+        # Get the ids that overlap with gold standard (if any)
+        overlapping_ids = coder_ids.intersection(gs_ids)
+
+        if len(overlapping_ids) > 0:
+            # Calculate the % agreement on the overlapping labels
+            tempdf = labels[labels['screenname'].isin([coder, EVAL_USER])]
+            tempdf = tempdf[tempdf['externalId'].isin(overlapping_ids)]
+            tempdf = tempdf.pivot(index='externalId', columns='screenname', values='labelText')
+            acc = accuracy_score(tempdf[EVAL_USER], tempdf[coder])
+        else:
+            acc = np.nan
+
+        # Get the stats on overlapping and total coded elements
+        out.append({'coder': coder,
+                    'gold_standard_accuracy': acc,
+                    'total': len(coder_ids),
+                    'unique': len(coder_ids.difference(gs_ids)),
+                    'overlap_gold_standard': len(overlapping_ids),})
+
+    sumdf = pd.DataFrame(out)
+    # Change col order and row index for nicer display
+    #sumdf = sumdf[['coder', 'total', 'unique', 'overlap_gold_standard']]
+    sumdf.set_index('coder', drop=True, inplace=True)
+    print_summary(f'Comparison to gold standard coder ({EVAL_USER})', sumdf)
+
+label_count = labels[['externalId', 'labelText']].groupby('externalId').count()
+multi_labeled_ids = label_count[label_count['labelText'] > 1].index
+multi_labeled = labels[labels['externalId'].isin(multi_labeled_ids)]
+
+multi_labeled = multi_labeled.pivot(index='externalId', columns='screenname', values='labelText')
 
 # Create the labels output
 
 ## Get the plurality vote for the overlapping elements
-eval_label_counts = eval_df.apply(lambda row: row.value_counts(), axis=1)
+eval_label_counts = multi_labeled.apply(lambda row: row.value_counts(), axis=1)
+
 eval_label_counts.fillna(0, inplace=True)
 eval_labels = pd.DataFrame(eval_label_counts.idxmax(axis=1), columns=['label'])
 eval_labels['entropy'] = eval_label_counts.apply(
@@ -152,10 +207,11 @@ eval_labels['entropy'] = eval_label_counts.apply(
 eval_labels.columns = ['label', 'entropy']
 
 ## Extract the labels for the rest of the data
-final_labels = labels[~labels['externalId'].isin(eval_ids)][['externalId', 'labelText']]
+final_labels = labels[~labels['externalId'].isin(multi_labeled_ids)][['externalId', 'labelText']]
 final_labels['entropy'] = np.nan
 final_labels.set_index('externalId', inplace=True)
 final_labels.columns = ['label', 'entropy']
 final_labels = pd.concat([eval_labels, final_labels])
 
+print(f'Storing output data in {OUTPATH}')
 final_labels.to_csv(OUTPATH)
