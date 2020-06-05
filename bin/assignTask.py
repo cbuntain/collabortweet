2b603cc5570fe71bde7ea27f19142c493b104640
cbuntain <cody@bunta.in>
2020-06-05 13:27:40 -0500

    Added Error check

--- bin/assignTask.py
+++ bin/assignTask.py
@@ -13,6 +13,7 @@ Authors: Cody Buntain, Fridolin Linder, Ian Rosenberg
 
 import sqlite3
 import argparse
+import sys
 
 if __name__ == '__main__':
 
@@ -30,8 +31,6 @@ if __name__ == '__main__':
     taskId = int(args.task_id)
     username = args.user
 
-    print("Restricting task views for %s  Task ID: [%d] from: %s" % (username, taskId, sqlitePath))
-
     # Open the sqlite3 file
     conn = sqlite3.connect(sqlitePath)
     c = conn.cursor()
@@ -41,6 +40,13 @@ if __name__ == '__main__':
     c.execute("SELECT * FROM users WHERE screenname = ?", param)
 
     row = c.fetchall()
+
+    if ( len(row) == 0 ):
+        print("No user with screen name:", username)
+        sys.exit(-1)
+
+
+    print("Restricting task views for %s  Task ID: [%d] from: %s" % (username, taskId, sqlitePath))
 
     userID = row[0][0]
 
